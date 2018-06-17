040313cd47def977361b423b672adbfc23f7ca95
onlymash <im@mash.im>
2018-06-17 22:26:43 +0800

    Update user agent

--- app/src/main/java/com/hippo/okhttp/ChromeRequestBuilder.java
+++ app/src/main/java/com/hippo/okhttp/ChromeRequestBuilder.java
@@ -23,8 +23,7 @@ import okhttp3.Request;
 
 public class ChromeRequestBuilder extends Request.Builder {
 
-    private static final String CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36";
+    private static final String CHROME_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36";
 
     public ChromeRequestBuilder(String url) throws MalformedURLException {
         url(new URL(url));
